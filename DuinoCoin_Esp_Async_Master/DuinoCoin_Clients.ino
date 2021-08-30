a44a4c4a97b6767fa792887ccadea9b4c0e7e9cf
JK-Rolling <89238608+JK-Rolling@users.noreply.github.com>
2021-08-30 21:19:06 +0800

    send slave reset request when see BAD 3 times

--- DuinoCoin_Esp_Async_Master/DuinoCoin_Clients.ino
+++ DuinoCoin_Esp_Async_Master/DuinoCoin_Clients.ino
@@ -321,6 +321,8 @@ void clients_waitFeedbackJobDone(byte i)
         Serial.println("BAD BAD BAD BAD");
         ws_sendAll("[" + String(i) + "]" + "BAD BAD BAD BAD");
         clients_stop(i);
+        //jk
+        wire_reset(i+1);
       }
     }
     else
